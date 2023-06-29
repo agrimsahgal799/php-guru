680a8385b571d11123ea45d361764b3cc16803c5
agrimsahgal799 <sahgal.agrim@gmail.com>
2023-06-29 13:26:00 +0530

    array diff assoc

--- array_diff_assoc.php
+++ array_diff_assoc.php
@@ -1,14 +1,24 @@
 <?php
 
-$a1 = array("a"=>"red","b"=>"green","c"=>"blue");
-$a2 = array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a1 = array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2 = array("a"=>"red","b"=>"green","c"=>"blue");
 
 $result = array_diff_assoc($a1,$a2);
-echo '<pre>';
-print_r($result);
+/*
+    Array
+    (
+        [d] => yellow
+    )
+*/
 
+/* get only repeated value using array unique */
 $arr = array(7,5,5,2,2,1);
-
 $result1 = array_unique(array_diff_assoc($arr, array_unique($arr)));
-echo '<pre>';
-print_r($result1);
+$result1 = array_values($result1);
+/*
+    Array
+    (
+        [2] => 5
+        [4] => 2
+    )
+*/
